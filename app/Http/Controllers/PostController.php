5e35ac7232500a6393da9803c9273a4eac07ce1c
nirobhasan-git <papon.nirob@gmail.com>
2023-11-25 08:44:45 +0600

    Create Update Delete Post & Route-Update-Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,24 +8,19 @@ use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
-    // public function index()
-    // {
-    //     $posts = Post::with('user')->paginate(5);
-    //     return view('posts.index', ['posts' => $posts]);
-    // }
     public function index(Request $request)
-{
-    $search = $request->input('search');
+    {
+        $search = $request->input('search');
 
-    $posts = Post::with('user')
-        ->when($search, function ($query, $search) {
-            return $query->where('title', 'like', '%' . $search . '%')
-                ->orWhere('description', 'like', '%' . $search . '%');
-        })
-        ->paginate(5);
+        $posts = Post::with('user')
+            ->when($search, function ($query, $search) {
+                return $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+            })
+            ->paginate(5);
 
-    return view('posts.index', ['posts' => $posts, 'search' => $search]);
-}
+        return view('posts.index', ['posts' => $posts, 'search' => $search]);
+    }
 
 
     public function show(Post $post)
@@ -33,6 +28,27 @@ class PostController extends Controller
         $this->authorize('view', $post);
         return view('posts.show', ['post' => $post]);
     }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:50',
+            'description' => 'required',
+        ]);
+
+        $post = Post::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ]);
+
+        return redirect(route('posts.index'))->with('status', 'Post created successfully.');
+    }
+
 
     public function destroy(Post $post)
     {
@@ -43,32 +59,26 @@ class PostController extends Controller
 
     public function edit(Post $post)
     {
-        if (!Gate::allows('isSuperAdmin', $post)) {
-            abort(403);
-        }
-        //
+        $this->authorize('update', $post);
+    
+        return view('posts.edit', ['post' => $post]);
+        dd($post);
     }
 
     public function update(Request $request, Post $post)
     {
-        if (!Gate::allows('isSuperAdmin', $post)) {
-            abort(403);
-        }
-        //
+        $this->authorize('update', $post);
+
         $request->validate([
-            'title' => 'required|string|max:155',
+            'title' => 'required|string|max:50',
             'description' => 'required',
         ]);
 
-        $updatepost = $post->update([
+        $post->update([
             'title' => $request->get('title'),
-            'description' => $request->get('description')
+            'description' => $request->get('description'),
         ]);
 
-        if($updatepost == !null){
-            return redirect(route('posts.index'))->with('status','Updated Complete');
-        }else{
-            return redirect(route('posts.index'))->withErrors(['message' => 'Can not update.']);
-        }
+        return redirect(route('posts.index'))->with('status', 'Post updated successfully');
     }
 }
