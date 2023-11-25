1fb18011342c30c0f7c9cd787f95815f1022d373
nirobhasan-git <papon.nirob@gmail.com>
2023-11-25 10:17:26 +0600

    Sanctum API with Routes for Postman

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
     public function index(Request $request)
     {
         $search = $request->input('search');
