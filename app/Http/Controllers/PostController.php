d272ff37941d1ca528472aa0e223b90471b64b0e
nirobhasan-git <papon.nirob@gmail.com>
2023-11-25 06:14:35 +0600

    Without middleware Use authorize to protect Route in controller & Search

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,17 +4,71 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        $posts = Post::with('user')->get();
-        return view('posts.index', ['posts' => $posts]);
-    }
+    // public function index()
+    // {
+    //     $posts = Post::with('user')->paginate(5);
+    //     return view('posts.index', ['posts' => $posts]);
+    // }
+    public function index(Request $request)
+{
+    $search = $request->input('search');
+
+    $posts = Post::with('user')
+        ->when($search, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('description', 'like', '%' . $search . '%');
+        })
+        ->paginate(5);
+
+    return view('posts.index', ['posts' => $posts, 'search' => $search]);
+}
+
 
     public function show(Post $post)
     {
+        $this->authorize('view', $post);
         return view('posts.show', ['post' => $post]);
+    }
+
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete', $post);
+        $post->delete();
+        return redirect(route('posts.index'));
+    }
+
+    public function edit(Post $post)
+    {
+        if (!Gate::allows('isSuperAdmin', $post)) {
+            abort(403);
+        }
+        //
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        if (!Gate::allows('isSuperAdmin', $post)) {
+            abort(403);
+        }
+        //
+        $request->validate([
+            'title' => 'required|string|max:155',
+            'description' => 'required',
+        ]);
+
+        $updatepost = $post->update([
+            'title' => $request->get('title'),
+            'description' => $request->get('description')
+        ]);
+
+        if($updatepost == !null){
+            return redirect(route('posts.index'))->with('status','Updated Complete');
+        }else{
+            return redirect(route('posts.index'))->withErrors(['message' => 'Can not update.']);
+        }
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,15 +4,33 @@
 <div class="container">
     <h3>{{ Auth::user()->name }}</h3>
     <hr>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}">Dashboard</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('posts.index') }}">All Posts</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">{{ $post->title }}</h5>
-                  <p class="card-text">{{ $post->description }}</p>
-                  <a href="{{ route('posts.index') }}" class="btn btn-primary">Go Back</a>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <p class="card-text">{{ $post->description }}</p>
+                    <a href="{{ route('posts.index') }}" class="btn btn-info">Go Back</a>
                 </div>
-              </div>
+            </div>
         </div>
     </div>
 </div>
